08f30bac848b919e3a964bcef95051e37329759b
DDD2308 <141920527+DDD2308@users.noreply.github.com>
2024-07-29 14:03:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function(){
     $('.carousel').slick({
-        slidesToShow: 2, // Show 2 cards at a time
-        slidesToScroll: 1, // Scroll 1 card at a time
-        arrows: true, // Show navigation arrows
-        infinite: true, // Infinite looping
-        dots: true, // Show dots for navigation
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true, // Enables navigation arrows
+        infinite: true,
+        dots: true,
         responsive: [
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 1, // Show 1 card at a time on small screens
+                    slidesToShow: 1,
                     slidesToScroll: 1
                 }
             }
