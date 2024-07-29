36cc250117cbbeaa9ce368e8b7082dd0f19e6664
DDD2308 <141920527+DDD2308@users.noreply.github.com>
2024-07-29 19:31:12 +0530

    Update script.js
    adaptive height and center padding

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 $(document).ready(function(){
     $('.carousel').slick({
+        centerMode: true,       // Enable center mode
+        centerPadding: '0px',
         slidesToShow: 2,
         slidesToScroll: 1,
         arrows: true, // Enables navigation arrows
         infinite: true,
         dots: true,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
+        nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
         responsive: [
             {
                 breakpoint: 768,
@@ -15,12 +20,6 @@ $(document).ready(function(){
             }
         ]
     });   
-$('.your-class').slick({
-    dots: true,
-    arrows: true,
-    prevArrow: '<button type="button" class="slick-prev">&#10094;</button>',
-    nextArrow: '<button type="button" class="slick-next">&#10095;</button>',
-});
 });
 
 
